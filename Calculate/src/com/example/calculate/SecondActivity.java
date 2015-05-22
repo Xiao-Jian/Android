57df9df7a5d0272a9e0c34e497f5a3169697c638
XiaoJian <1215615213@qq.com>
2015-05-22 18:44:25 +0800

    new

---
+++ Calculate/src/com/example/calculate/SecondActivity.java
@@ -0,0 +1,11 @@
+package com.example.calculate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SecondActivity extends Activity {
+	protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_second);
+    }
+}
